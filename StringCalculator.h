999b96ed884a04883f115c95c8044dc871eb985f
Nivedita Naik <116743692+Nivedita-Naik@users.noreply.github.com>
2024-09-18 20:36:50 +0530

    Update StringCalculator1.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,75 +1,64 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
- 
-bool detect_custom_delimiter(const char* str) {
-    return str[0] == '/' && str[1] == '/';
-}
- 
-void get_custom_delimiter(const char* str, char* delimiter) {
-    const char* start = str + 2; // Skip over "//"
-    size_t length = strcspn(start, "\n"); // Find the position of the newline character
-    strncpy(delimiter, start, length); // Copy the delimiter
-    delimiter[length] = '\0'; // Null-terminate the delimiter
-}
- 
-void tokenize_numbers(const char* str, const char* delimiters, int* nums, int* num_count) {
-    char* copy = strdup(str);
-    char* token = strtok(copy, delimiters);
-    while (token) {
-        nums[(*num_count)++] = atoi(token);
-        token = strtok(NULL, delimiters);
-    }
-    free(copy);
-}
- 
-bool find_negatives(int* nums, int num_count, char* error_msg) {
-    bool has_negatives = false;
-    strcpy(error_msg, "negatives not allowed: ");
-    for (int i = 0; i < num_count; i++) {
-        if (nums[i] < 0) {
-            has_negatives = true;
-            char num_str[12];
-            snprintf(num_str, sizeof(num_str), "%d ", nums[i]);
-            strcat(error_msg, num_str);
-        }
-    }
-    return has_negatives;
-}
- 
-int sum_numbers(int* nums, int num_count) {
-    int total = 0;
-    for (int i = 0; i < num_count; i++) {
-        if (nums[i] <= 1000) {
-            total += nums[i];
-        }
-    }
-    return total;
-}
- 
-int process_and_sum(const char* str, const char* delimiters) {
-    int nums[1000];
-    int num_count = 0;
-    tokenize_numbers(str, delimiters, nums, &num_count);
- 
-    char error_msg[256];
-    if (find_negatives(nums, num_count, error_msg)) {
-        return -1; // Indicate an error
-    }
- 
-    return sum_numbers(nums, num_count);
-}
- 
-int add(const char* str) {
-    if (*str == '\0') {
+
+int isNumGreaterThanThousand(int num)
+{
+    if(num > 1000)
+    {
         return 0;
     }
- 
-    char delimiter[10] = ",\n";
-    if (detect_custom_delimiter(str)) {
-        get_custom_delimiter(str, delimiter);
+    return num;
+}
+
+int tokenizeString(char *input)
+{
+    char *token;
+    int total = 0;
+    const char delimiters[] = "$#\n:,;//\\&#%";
+
+    token = strtok(input, delimiters);
+    while (token != NULL) 
+    {
+        // Convert token to integer using atoi
+        int num = atoi(token);
+
+        total += isNumGreaterThanThousand(num);
+        // Get the next token
+        token = strtok(NULL, delimiters);
     }
- 
-    return process_and_sum(str, delimiter);
+
+    return total;
+}
+
+int createCopy(const char *input)
+{
+    if (strlen(input) == 1)
+    {
+        return atoi(input);
+    }
+    // Create a modifiable copy of the input
+    char inputCopy[100];
+    strcpy(inputCopy, input);
+    return tokenizeString(inputCopy); // Provide sum only if it is not a single character
+}
+
+int isEmptyString(const char* input)
+{
+  if (input == NULL || input[0] == '\0') 
+  {
+     return 1; 
+  }
+  return 0;
+}
+
+int add (const char* str)
+{
+  if(isEmptyString(str))
+  {
+     return 0;
+  }
+  else
+  {
+    return createCopy(str);
+  }
 }
